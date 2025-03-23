6ede278232de43eda872bd40d07c5bd3b5763cf5
Benjamin Nicholls <nicholls.ben@gmail.com>
2025-03-22 22:48:33 -0400

    api updates

--- startmenu.go
+++ startmenu.go
@@ -18,10 +18,8 @@ type StartMenu struct {
 func (sm *StartMenu) Init() {
 	sm.State.Init()
 
-	title := ui.Textbox{}
-	title.Init(vec.Dims{ui.FIT_TEXT, 1}, vec.Coord{0, 10}, 1, "SPACE SHIPPERS: The Ones Who Space Ship!", ui.JUSTIFY_CENTER)
-	title.EnableBorder()
-	sm.Window().AddChild(&title)
+	title := ui.NewTitleTextbox(vec.Dims{ui.FIT_TEXT, 1}, vec.Coord{0, 10}, 1, "SPACE SHIPPERS: The Ones Who Space Ship!")
+	sm.Window().AddChild(title)
 	title.CenterHorizontal()
 
 	sm.menu.Init(vec.Dims{16, 5}, vec.Coord{0, 0}, 1)
@@ -35,41 +33,34 @@ func (sm *StartMenu) Init() {
 	sm.stars.Init(vec.Dims{96, 54}, vec.ZERO_COORD, 0, 25, 10)
 	sm.Window().AddChildren(&sm.stars)
 
-	sm.SetInputHandler(sm.HandleInput)
+	sm.SetKeypressHandler(sm.HandleKeypress)
 }
 
-func (sm *StartMenu) HandleInput(input_event event.Event) (event_handled bool) {
-	if input_event.Handled() {
+func (sm *StartMenu) HandleKeypress(key_event *input.KeyboardEvent) (event_handled bool) {
+	if key_event.Handled() {
 		return
 	}
 
-	if input_event.ID() == input.EV_KEYBOARD {
-		key_event := input_event.(*input.KeyboardEvent)
-		if key_event.PressType == input.KEY_RELEASED {
-			return
+	switch key_event.Key {
+	case input.K_RETURN:
+		switch sm.menu.GetSelectionIndex() {
+		case 0: //New Game
+			tyumi.ChangeState(NewCreateGalaxyMenu())
+		case 1: //Load Game
+			//Load Game dialog
+		case 2: //Ship Designer
+			tyumi.ChangeState(NewShipDesignMenu())
+		case 3: //Options
+			//Options Dialog
+		case 4: //Quit
+			event.FireSimple(tyumi.EV_QUIT)
 		}
-
-		switch key_event.Key {
-		case input.K_RETURN:
-			switch sm.menu.GetSelectionIndex() {
-			case 0: //New Game
-				tyumi.ChangeState(NewCreateGalaxyMenu())
-			case 1: //Load Game
-				//Load Game dialog
-			case 2: //Ship Designer
-				tyumi.ChangeState(NewShipDesignMenu())
-			case 3: //Options
-				//Options Dialog
-			case 4: //Quit
-				event.FireSimple(tyumi.EV_QUIT)
-			}
-		case input.K_SPACE: //FOR TESTING PURPOSES ONLY DAMMIT
-			// g := NewGalaxy("Test Galaxy", GAL_MAX_RADIUS, GAL_DENSE)
-			// s := NewShip("The Greatest Spaceship There Is", g)
-			// temp, _ := LoadShipTemplate("raws/ship/Transport Ship.shp")
-			// s.SetupFromTemplate(temp)
-			// burl.ChangeState(NewSpaceshipGame(g, s))
-		}
+	case input.K_SPACE: //FOR TESTING PURPOSES ONLY DAMMIT
+		// g := NewGalaxy("Test Galaxy", GAL_MAX_RADIUS, GAL_DENSE)
+		// s := NewShip("The Greatest Spaceship There Is", g)
+		// temp, _ := LoadShipTemplate("raws/ship/Transport Ship.shp")
+		// s.SetupFromTemplate(temp)
+		// burl.ChangeState(NewSpaceshipGame(g, s))
 	}
 
 	return
